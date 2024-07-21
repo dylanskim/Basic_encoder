b822a4cc19e804180f1e1f5815274910759dfdc2
IX <52508423+dylanskim@users.noreply.github.com>
2024-07-21 19:50:23 -0400

    Update Basic_encoder.py

--- Basic_encoder.py
+++ Basic_encoder.py
@@ -7,15 +7,13 @@ if not user_in == "":
     text_ascii = map(str, list(map(ord, user_in)))
     spaced_text_ascii = int(join_delimiter.join(text_ascii))
     output_encoded = str(multi * spaced_text_ascii)
-    print(output_encoded + join_delimiter + str(multi))
+    print(hex(int(output_encoded + join_delimiter + str(multi))))
 
 else:
-    user_code = input("Type encoded text here: ")
-    if not user_code.isdigit():
-        print("Not Encoded")
-    else:
-        code_decode = str(int(user_code[0:-5]) // int(user_code[-1]))
-        code_delimiter = user_code[-5:-1]
-        code_translate = [int(i) for i in code_decode.split(code_delimiter)]
-        translate_output = [chr(i) for i in code_translate]
-        print("".join(translate_output))
+    user_hex = input("Type encoded text here: ")
+    user_code = str(int(user_hex,0))
+    code_decode = str(int(user_code[0:-5]) // int(user_code[-1]))
+    code_delimiter = user_code[-5:-1]
+    code_translate = [int(i) for i in code_decode.split(code_delimiter)]
+    translate_output = [chr(i) for i in code_translate]
+    print("".join(translate_output))
